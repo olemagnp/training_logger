dcb201d7a6047dc47a35df1ede8c356ff0d4a74e
Ole-Magnus Pedersen <pedersen.olemagnus@gmail.com>
2021-03-12 13:40:01 +0100

    Add __version__

--- training_logger/__init__.py
+++ training_logger/__init__.py
@@ -1,4 +1,5 @@
 from .training_logger import TrainingLogger
 from .visualizers import LogVisualizer, MultiLogVisualizer
 
-__all__ = ['TrainingLogger', 'LogVisualizer', 'MultiLogVisualizer']
+__all__ = ["TrainingLogger", "LogVisualizer", "MultiLogVisualizer"]
+__version__ = "0.1.7"
